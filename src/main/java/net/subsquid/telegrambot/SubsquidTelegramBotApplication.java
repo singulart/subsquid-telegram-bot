fd0d131ccbbf962d2ef6ab5e4b7fd72dafa16238
singulart <serpian@gmail.com>
2022-01-14 15:28:56 +0200

    initial

---
+++ src/main/java/net/subsquid/telegrambot/SubsquidTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package net.subsquid.telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubsquidTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubsquidTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/subsquid/telegrambot/SubsquidTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package net.subsquid.telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubsquidTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
