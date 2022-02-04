669b1c6e25aa5791837e1e7a3ff5ee1099b889b7
singulart <serpian@gmail.com>
2022-02-04 14:44:59 +0200

    Fixed too long Telegram message

--- src/main/java/net/subsquid/telegrambot/beans/TelegramNotifierJob.java
+++ src/main/java/net/subsquid/telegrambot/beans/TelegramNotifierJob.java
@@ -3,11 +3,12 @@ package net.subsquid.telegrambot.beans;
 import static java.lang.String.format;
 import static net.subsquid.telegrambot.Constants.CHANNEL;
 
+import com.google.common.collect.Lists;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.request.ParseMode;
 import com.pengrad.telegrambot.request.SendMessage;
-import com.pengrad.telegrambot.response.SendResponse;
 import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import net.subsquid.telegrambot.SubsquidIndexerStatus;
@@ -26,6 +27,9 @@ public class TelegramNotifierJob implements Job {
     @Value("${data.feed.url}")
     private String dataFeedURL;
 
+    @Value("${batch.size}")
+    private Integer batchSize;
+
     private final RestTemplate restTemplate;
     private final TelegramBot bot;
     private final String telegramChannelId = System.getProperty(CHANNEL);
@@ -41,19 +45,23 @@ public class TelegramNotifierJob implements Job {
         log.info("Executing the job");
         var responseObject = restTemplate.getForObject(dataFeedURL, SubsquidIndexerStatus[].class);
         if(responseObject != null) {
-            String chatNotificationText = responseToChatMessage(responseObject);
-            SendResponse sendResponse = bot.execute(
-                new SendMessage(telegramChannelId, chatNotificationText).parseMode(ParseMode.MarkdownV2)
-            );
-            log.info(sendResponse.toString());
+            var batches = Lists.partition(Arrays.asList(responseObject), batchSize);
+            for(var batch: batches) {
+                log.info("Sending batch");
+                var chatNotificationText = responseToChatMessage(batch);
+                var sendResponse = bot.execute(
+                    new SendMessage(telegramChannelId, chatNotificationText).parseMode(ParseMode.MarkdownV2)
+                );
+                log.info(sendResponse.toString());
+            }
         } else {
             log.warning("Did not receive indexer list");
         }
     }
 
-    private String responseToChatMessage(SubsquidIndexerStatus[] responseObject) {
-        String header = "Subsquid Indexers: \n";
-        String body = Arrays.stream(responseObject).map((status) -> format("*Network: %s*\n"
+    private String responseToChatMessage(List<SubsquidIndexerStatus> responseObject) {
+        var header = "Subsquid Indexers: \n";
+        var body = responseObject.stream().map((status) -> format("*Network: %s*\n"
             + "```\nURL: %s\n"
             + "Hydra Version: %s\n"
             + "Synced: %s\n"
